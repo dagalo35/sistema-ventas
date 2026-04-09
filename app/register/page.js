ede186040def658d84fcba73600a2a9126492c16
Manuel <gaedom123@gmail.com>
2026-04-09 14:09:28 -0500

    fix build register page

--- app/register/page.js
+++ app/register/page.js
@@ -2,7 +2,6 @@
 
 export const dynamic = "force-dynamic"
 export const fetchCache = "force-no-store"
-export const revalidate = 0
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
